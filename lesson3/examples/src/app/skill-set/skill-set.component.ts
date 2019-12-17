bf28b0f3459e5b5ad84da9f46fe2b862c5c3c131
keitaroryu <keitaroryu@gmail.com>
2019-12-17 16:13:09 -0600

    Fixing lesson 3 merge

--- lesson3/examples/src/app/skill-set/skill-set.component.ts
+++ lesson3/examples/src/app/skill-set/skill-set.component.ts
@@ -8,13 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class SkillSetComponent implements OnInit {
    listHeading: string = 'Some Coding Skills I Know';
    skills: string[] = ['Loops', 'Conditionals', 'Functions', 'Classes', 'Modules', 'Git', 'HTML/CSS'];
-<<<<<<< HEAD
    alternateColor: string = 'black';
    bulletType: string = 'A';
-=======
-   bulletType: string = 'A';
-   alternateColor: string = 'black';
->>>>>>> 9fd9381bf8a2523e0db6d36f797a4b9ccc94cf53
    changeColor: boolean = true;
 
    constructor() { }
